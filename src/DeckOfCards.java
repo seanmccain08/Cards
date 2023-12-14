9b74fbd50a3582abd18c3aed7ac53d1705beb427
Sean McCain <seanmccain327@gmail.com>
2023-12-14 08:10:31 -0600

    Replaced array with an arraylist. Added ability to deal the whole deck.

--- src/DeckOfCards.java
+++ src/DeckOfCards.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DeckOfCards{
 
     /*
@@ -6,17 +8,21 @@ public class DeckOfCards{
     3: Diamonds
     4: Clubs
     */
-    private Card[] deck0;
+    private ArrayList<Card>deck0;
+    public static ArrayList<Card>hand1 = new ArrayList<Card>();
+    public static ArrayList<Card>hand2 = new ArrayList<Card>();
+    public static ArrayList<Card>hand3 = new ArrayList<Card>();
+    public static ArrayList<Card>hand4 = new ArrayList<Card>();
 
     public DeckOfCards(){
 
-        deck0 = new Card[52];
+        deck0 = new ArrayList<Card>(52);
         int id = 0;
         for(int i = 0; i < 4; i++){
 
             for(int j = 0; j < 13; j++, id++){
 
-                deck0[id] = new Card(j+1, i+1);
+                deck0.add(new Card(j+1, i+1));
 
             }
 
@@ -26,26 +32,138 @@ public class DeckOfCards{
     public String toString(){
 
         String result = "";
-        for(int i = 0; i < 52; i++){
+        if(deck0.size() == 0){
 
-            result += deck0[i].toString()+"\n";
+            result+="The whole deck has already been dealt.";
 
+        }
+        else {
+            for (int i = 0; i < deck0.size(); i++) {
+
+                result += deck0.get(i).toString() + "\n";
+
+            }
         }
         return result;
 
     }
     public void shuffle(){
 
-        for(int i = 0; i < 52; i++){
+        if(deck0.size() < 52){
 
-            int rand = (int)(Math.random()*51+1);
-            Card temp = deck0[rand];
-            deck0[rand] = deck0[i];
-            deck0[i] = temp;
+            System.out.println("Cards have already been dealt. To shuffle, create a new deck.");
 
         }
-        System.out.println("Deck successfully shuffled!");
+        else {
+            for (int i = 0; i < 52; i++) {
+
+                int rand = (int) (Math.random() * 51 + 1);
+                Card temp = deck0.get(rand);
+                deck0.set(rand, deck0.get(i));
+                deck0.set(i, temp);
+
+            }
+            System.out.println("Deck successfully shuffled!");
+        }
 
     }
+    public void deal(int players){
+
+        switch(players){
+
+            case 2:
+                for(int i = 0, player = 0; i<52; i++){
+
+                    if(player == 0){
+
+                        player = 1;
+                        hand1.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else{
+
+                        player = 0;
+                        hand2.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+
+                }
+                System.out.println("Successfully dealt the deck.");
+                break;
+
+            case 3:
+                for(int i = 0, player = 0; i<52; i++){
+
+                    if(player == 0){
+
+                        player = 1;
+                        hand1.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else if(player == 1){
+
+                        player = 2;
+                        hand2.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else{
+
+                        player = 0;
+                        hand3.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+
+                }
+                System.out.println("Successfully dealt the deck.");
+                break;
+
+            case 4:
+                for(int i = 0, player = 0; i<52; i++){
+
+                    if(player == 0){
+
+                        player = 1;
+                        hand1.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else if(player == 1){
+
+                        player = 2;
+                        hand2.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else if(player == 2){
+
+                        player = 3;
+                        hand3.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+                    else{
+
+                        player = 0;
+                        hand3.add(deck0.get(0));
+                        deck0.remove(0);
+
+                    }
+
+                }
+                System.out.println("Successfully dealt the deck.");
+                break;
+
+            default:
+                System.out.println("Please enter a valid amount of players.");
+
+        }
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -14,28 +14,47 @@ class Main {
 
         while(playing){
 
-            System.out.println("1. View Deck\n2. Shuffle Deck\n3. Deal all cards...\n4. Deal a specified amount of cards...\n5. Quit");
+            System.out.println("1. Create a deck...\n2. View Deck\n3. Shuffle Deck\n4. Deal all cards...\n5. Deal a specified amount of cards...\n6. Quit");
             int choice = scan.nextInt();
             switch(choice){
 
                 case 1:
-                    System.out.println(deck.toString());
+                    deck = new DeckOfCards();
                     break;
 
                 case 2:
-                    deck.shuffle();
+                    System.out.println(deck.toString());
                     break;
 
                 case 3:
+                    deck.shuffle();
                     break;
 
                 case 4:
+                    System.out.println("How many players? (2-4)");
+                    choice = scan.nextInt();
+                    deck.deal(choice);
                     break;
 
                 case 5:
-
-                    System.out.println("Are you sure you want to quit?");
                     break;
+
+                case 6:
+                    System.out.println("Are you sure you want to quit?");
+                    System.out.println("1. Yes\n2. No");
+                    choice = scan.nextInt();
+                    if (choice == 1) {
+
+                        playing = false;
+                        System.out.println("Quitting...");
+                        break;
+
+                    }
+                    else{
+
+                        break;
+
+                    }
 
                 default:
                     System.out.println("Please make a valid choice.");
